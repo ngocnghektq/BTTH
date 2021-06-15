76f79c6a03b0943691623469480df613247bcee0
ngocnghektq <mathiduyen2403@gmail.com>
2021-06-15 10:24:13 +0700

    Bài kiểm tra

--- Buoi 16/src/main/java/Main.java
+++ Buoi 16/src/main/java/Main.java
@@ -4,12 +4,11 @@ import kiemtra.Bai3;
 
 public class Main {
     public static void main(String[] args) {
-        //Bai3 bai3= new Bai3();
-        // bai3.nhapSo();
-        // Bai1 bai1= new Bai1();
-        // bai1.demTu();
+        Bai3 bai3= new Bai3();
+         bai3.nhapSo();
+         Bai1 bai1= new Bai1();
+         bai1.demTu();
         Bai2 bai2 = new Bai2();
-        //bai2.nhapMK();
         String passWord = bai2.nhapMK();
         if (bai2.checkLength(passWord)) {
             System.out.println("Mật khẩu yếu.");
--- Buoi 16/src/main/java/kiemtra/Bai3.java
+++ Buoi 16/src/main/java/kiemtra/Bai3.java
@@ -1,24 +1,34 @@
 package kiemtra;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Bai3 {
-    public void nhapSo()
-    {
-        int n;
-        System.out.println("Nhap vao một số: ");
-        Scanner scn = new Scanner(System.in);
-        n = scn.nextInt();
-      //  void cachIfElse(int n) {
-        if (n == 1 | n == 3 | n == 5 | n == 7 | n == 8 | n == 10 | n == 12) {
-            System.out.println("Thang: " + n +  " là tháng trong năm và" + " co 31 ngay");
-        } else if (n == 2) {
-            System.out.println("Thang: " + n + " là tháng trong năm và" + " co 28 hoac 29 ngay");
-        } else if (n == 4 | n == 6 | n == 9 | n == 11) {
-            System.out.println("Thang: " + n + " là tháng trong năm và" + " co 30 ngay");
-        } else {
-            System.out.println("Số đó không phải là tháng");
+    public void nhapSo() {
+
+        try {
+            int n;
+            System.out.println("Nhap vao một số: ");
+            Scanner scn = new Scanner(System.in);
+            n = scn.nextInt();
+
+            //  void cachIfElse(int n) {
+            if (n == 1 | n == 3 | n == 5 | n == 7 | n == 8 | n == 10 | n == 12) {
+                System.out.println("Thang: " + n + " là tháng trong năm và" + " co 31 ngay");
+            } else if (n == 2) {
+                System.out.println("Thang: " + n + " là tháng trong năm và" + " co 28 hoac 29 ngay");
+            } else if (n == 4 | n == 6 | n == 9 | n == 11) {
+                System.out.println("Thang: " + n + " là tháng trong năm và" + " co 30 ngay");
+            } else {
+                System.out.println("Số đó không phải là tháng");
+            }
+        }
+        catch (NegativeArraySizeException e1){
+            System.out.println("Phai nhap so > 0");
+        }
+        catch (InputMismatchException e2){
+            System.out.println("Gia tri nhap vao phai la so");
         }
     }
-    }
+}
 
